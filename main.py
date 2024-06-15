0c2a2825d771db0b1500977e5ebb477c74e8068d
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-06-15 14:45:12 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# x = input()
+# unique = 0
+# letters = set()
+# for i in x:
+#     if i not in letters:
+#         letters.add(i)
+#
+# l = len(letters)
+# if l % 2 == 0:
+#     print('CHAT WITH HER!')
+# else:
+#     print('IGNORE HIM!')
